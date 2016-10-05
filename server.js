bf3cf1a283f4ed1a99ba3ebdcdb427edfae90c43
FLOURA <floura.angel.96@gmail.com>
2016-10-06 02:22:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var PersonalDetails={
+var Details ={
+    
+'personal-details':{
     title:'Personal Details',
     heading:'Personal Details',
     content:`<div>
@@ -23,7 +25,63 @@ var PersonalDetails={
             EditAstronomy, Coding, Travelling, Volunteer Work, Reading
             <br>
             </div>`
-};
+},
+'achievements' : {
+    title:'Achievements Details',
+    heading:'Achievements Details',
+    content:`<div>
+            <br>
+            <br>
+            <h4>College:</h4>
+               Award of Academic Proficiency,<br>
+               Karunya University,<br>
+               December 2015
+            <br>
+            </div>
+            <div>
+            <br>
+            <h4>Certifications:</h4>
+                Oracle Certified Java SE 6 Programmer,<br>
+                Oracle,<br>
+                May 2016,<br>
+            <br>
+                Business English Certificate Vantage,<br>
+                Cambridge English,<br>
+                November 2014,<br>
+            <br>
+                Core Java,<br>
+                M-CAT Computer Education,<br>
+                June 2015,<br>
+            <br>
+            </div>`
+},
+'experiences' : {
+    title:'Experience Details',
+    heading:'Experience Details',
+    content:`<div>
+            <br>
+            <br>
+            <h4>Summer Internship:</h4>
+               Designed the Progen ERP website using WordPress
+               <a href="http://progenerp.com/">Progen ERP</a>
+            <br>
+            </div>
+            <div>
+            <br>
+            <h4>Current Projects:</h4>
+                Automation of Conventational Target Paln,<br>
+                Karunya University,<br>
+            <br>
+                Land Marking using Android,<br>
+                Karunya University,<br>
+            <br>
+                Smart Traffic Control using IP networks,<br>
+                Karunya University,<br>
+            <br>
+            </div>`
+}
+
+} ;
 
 function createTemplate(data){
     var title = data.title;
@@ -61,16 +119,17 @@ app.get('/home', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/personal-details', function (req, res) {
-  res.send(createTemplate(PersonalDetails));
+app.get('/:DetailsName', function (req, res) {
+  var DetailsName = req.params.DetailsName;
+  res.send(createTemplate(Details[DetailsName]));
 });
 
 app.get('/achievements', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send(createTemplate(PersonalDetails));
 });
 
 app.get('/experiences', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send(createTemplate(PersonalDetails));
 });
 
 app.get('/ui/style.css', function (req, res) {
