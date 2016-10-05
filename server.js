b7893bc46a918bcbead9266b32f02e82dead7553
FLOURA <floura.angel.96@gmail.com>
2016-10-06 01:58:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var PersonalDetails={
+    title:'Personal Details',
+    heading:'Personal Details',
+    content:`<div>
+            <br>
+            <br>
+            <h4>Motivation:</h4>
+                Driven by the idea to revolutionise the world with innovative technology, I am a confident 
+                and passionate individual with a strong technical background 
+                looking forward to learn new things and explore myself.
+            <br>
+            </div>
+            <div>
+            <br>
+            <h4>Interests:</h4>
+            EditAstronomy, Coding, Travelling, Volunteer Work, Reading
+            <br>
+            </div>`
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    
+        var HTMLTemplate = `
+        <html>
+        <head>
+        <title>
+        ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="pers">
+                <div>
+                    <h3>${heading}</h3>
+                </div>
+                ${content}
+            </div>
+        </body>
+        </html>
+        `;
+        
+        return HTMLTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +62,7 @@ app.get('/home', function (req, res) {
 });
 
 app.get('/personal-details', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'personal.html'));
+  res.send(createTemplate(PersonalDetails));
 });
 
 app.get('/achievements', function (req, res) {
