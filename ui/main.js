8a671e624a14f07d87192c939cf6164179659f69
FLOURA <floura.angel.96@gmail.com>
2016-10-08 22:40:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,19 @@
-console.log('Loaded!');
+var button = document.getElementById("counter");
 
-var marginLeft = 0;
-
-function moveRight(){
-    marginLeft = marginLeft+10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-var idel = document.getElementById("madi");
-idel.onclick = function(){
-    var interval = setInterval(moveRight,100);
-}
+button.onclick = function(){
+ 
+ var request = new XMLHttpRequest();
+ request.onreadystatechange = function(){
+     if(request.readyState === XMLHttpRequest.DONE){
+         if(request.state === 200){
+            var counter = request.responseText;  
+             var span = document.getElementById("counter");
+             span.innerHTML = counter.toString();
+         }
+     }
+ };
+ 
+ request.open('GEET','http://floura71196.imad.hasura-app.io//counter',true);
+ request.send(null);
+ 
+};
