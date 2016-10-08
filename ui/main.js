7fd9f1f4d53dcf3b34404fd8e831261bbf2a40e1
FLOURA <floura.angel.96@gmail.com>
2016-10-08 22:02:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var element = document.getElementById("main-text");
+element.innerHTML = "new content";
+
+var idel = document.getElementById("madi");
+idel.onclick = function(){
+    img.style.marginLeft = '100px';
+}
