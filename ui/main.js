46883a937f15b540bb17954e46be13db2b3be845
FLOURA <floura.angel.96@gmail.com>
2016-11-24 16:54:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,36 +1,112 @@
-var button = document.getElementById("counter");
+// var button = document.getElementById("counter");
 
-button.onclick = function(){
+// button.onclick = function(){
  
- var request = new XMLHttpRequest();
- request.onreadystatechange = function(){
-     if(request.readyState === XMLHttpRequest.DONE){
-         if(request.state === 200){
-            var counter = request.responseText;  
-             var span = document.getElementById("counter");
-             span.innerHTML = counter.toString();
-         }
-     }
- };
+//  var request = new XMLHttpRequest();
+//  request.onreadystatechange = function(){
+//      if(request.readyState === XMLHttpRequest.DONE){
+//          if(request.state === 200){
+//             var counter = request.responseText;  
+//              var span = document.getElementById("counter");
+//              span.innerHTML = counter.toString();
+//          }
+//      }
+//  };
  
- request.open('GEET','http://floura71196.imad.hasura-app.io//counter',true);
- request.send(null);
+//  request.open('GEET','http://floura71196.imad.hasura-app.io//counter',true);
+//  request.send(null);
  
-};
+// };
 
 
-//submit
+// //submit
 
-var nameInput = document.getElementById("name");
-var name1 = nameInput.value;
-var sub = document.getElementById("sub_but");
-sub.onclick = function(){
-    //capture name and render as list
-    var names = ['name1','name2'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list += '<li>'+names[i]  +  '</li>';
+// var nameInput = document.getElementById("name");
+// var name1 = nameInput.value;
+// var sub = document.getElementById("sub_but");
+// sub.onclick = function(){
+//     //capture name and render as list
+//     var names = ['name1','name2'];
+//     var list = '';
+//     for(var i=0;i<names.length;i++){
+//         list += '<li>'+names[i]  +  '</li>';
+//     }
+//     var ol = document.getElementById('nameList');
+//     ol.innerHTML = list
+// };
+
+function myhover1(){
+        document.getElementsByClassName("vischar")[0].style = "border: 5px solid black ; background-color:white";
     }
-    var ol = document.getElementById('nameList');
-    ol.innerHTML = list
-};
+function myhover2(){
+        document.getElementsByClassName("vischar")[1].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover3(){
+        document.getElementsByClassName("vischar")[2].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover4(){
+        document.getElementsByClassName("vischar")[3].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover5(){
+        document.getElementsByClassName("vischar")[4].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover6(){
+        document.getElementsByClassName("vischar")[5].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover7(){
+        document.getElementsByClassName("vischar")[6].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover8(){
+        document.getElementsByClassName("vischar")[7].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover9(){
+        document.getElementsByClassName("vischar")[8].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover10(){
+        document.getElementsByClassName("vischar")[9].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover11(){
+        document.getElementsByClassName("vischar")[10].style = "border: 5px solid black ; background-color:white";
+    }
+function myhover12(){
+        document.getElementsByClassName("vischar")[11].style = "border: 5px solid black ; background-color:white";
+    }
+function myhovera1(){
+        document.getElementsByClassName("vischar2")[0].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera2(){
+        document.getElementsByClassName("vischar2")[1].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera3(){
+        document.getElementsByClassName("vischar2")[2].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera4(){
+        document.getElementsByClassName("vischar2")[3].style = "background-color:azure;border:none";
+    }
+function myhovera5(){
+        document.getElementsByClassName("vischar2")[4].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera6(){
+        document.getElementsByClassName("vischar2")[5].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera7(){
+        document.getElementsByClassName("vischar2")[6].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera8(){
+        document.getElementsByClassName("vischar2")[7].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera9(){
+        document.getElementsByClassName("vischar2")[8].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera10(){
+        document.getElementsByClassName("vischar2")[9].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera11(){
+        document.getElementsByClassName("vischar2")[10].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera12(){
+        document.getElementsByClassName("vischar2")[11].style = "border: 2px solid black ; background-color:white";
+    }
+function myhovera13(){
+        document.getElementsByClassName("vischar2")[12].style = "border: 2px solid black ; background-color:white";
+    }
