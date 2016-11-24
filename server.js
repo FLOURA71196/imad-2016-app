76abe3304ca2c8b72fe6fc5d7df813181b8fc2f6
FLOURA <floura.angel.96@gmail.com>
2016-11-24 18:10:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,21 +10,66 @@ var Details ={
 'personal-details':{
     title:'Personal Details',
     heading:'Personal Details',
-    content:`<div>
-            <br>
-            <br>
-            <h4>Motivation:</h4>
-                Driven by the idea to revolutionise the world with innovative technology, I am a confident 
-                and passionate individual with a strong technical background 
-                looking forward to learn new things and explore myself.
-            <br>
-            </div>
-            <div>
-            <br>
-            <h4>Interests:</h4>
-            EditAstronomy, Coding, Travelling, Volunteer Work, Reading
-            <br>
-            </div>`
+    content:`<div id="navg">
+	<div class="menu">Home</div>
+	<div class="menu">Intro</div>
+	<div class="menu">MyResume</div>
+	<div class="menu">Family</div>
+	<div class="menu1">Blog</div>
+</div>
+<div class="spacing"></div>
+<div id="section1">
+    <div class="col1">
+        <img id="mypic" src="me2.jpg">
+    </div>
+    <div id="content">
+    <div class="col">
+        <div class="vischar" onmouseover="myhover1()">F</div>
+        <div class="vischar" onmouseover="myhover2()">L</div>
+        <div class="vischar" onmouseover="myhover3()">O</div>
+        <div class="vischar" onmouseover="myhover4()">U</div>
+        <div class="vischar" onmouseover="myhover5()">R</div>
+        <div class="vischar" onmouseover="myhover6()">A</div>
+        <div class="vischar" onmouseover="myhover7()">:)</div>
+        <div class="vischar" onmouseover="myhover8()">A</div>
+        <div class="vischar" onmouseover="myhover9()">N</div>
+        <div class="vischar" onmouseover="myhover10()">G</div>
+        <div class="vischar" onmouseover="myhover11()">E</div>
+        <div class="vischar" onmouseover="myhover12()">L</div>
+    </div>
+        <br>
+    <div class="col12">
+        <div class="vischar2" onmouseover="myhovera1()">W</div>
+        <div class="vischar2" onmouseover="myhovera2()">E</div>
+        <div class="vischar2" onmouseover="myhovera3()">B</div>
+        <div class="vischar2" onmouseover="myhovera4()">&nbsp;</div>
+        <div class="vischar2" onmouseover="myhovera5()">D</div>
+        <div class="vischar2" onmouseover="myhovera6()">E</div>
+        <div class="vischar2" onmouseover="myhovera7()">V</div>
+        <div class="vischar2" onmouseover="myhovera8()">E</div>
+        <div class="vischar2" onmouseover="myhovera9()">L</div>
+        <div class="vischar2" onmouseover="myhovera10()">O</div>
+        <div class="vischar2" onmouseover="myhovera11()">P</div>
+        <div class="vischar2" onmouseover="myhovera12()">E</div>
+        <div class="vischar2" onmouseover="myhovera13()">R</div>
+    </div>
+    </div>
+</div>
+    <br>
+    <br><br><br>
+    <div class="spacing"></div>
+    <div id="intro">
+    <h1>INTRODUCTION</h1><br>
+        <p>Driven by the idea to revolutionise the world with innovative technology,I am a confident and passionate individual with a strong technical background looking forward to learn new things and explore myself.</p>
+    </div><br>
+    <br><br><br>
+    <div class="spacing"></div>
+    <div id="resume">
+    <h1>MY RESUME</h1>
+        <h4>Technical Expertise, Academics, Certifications, Workshop Attended, Projects, Experience, Achievements, Strengths, Hobbies.
+        </h4>
+        <h5 id="readmore">read more..</h5>
+    </div>`
 },
 'achievements' : {
     title:'Achievements Details',
