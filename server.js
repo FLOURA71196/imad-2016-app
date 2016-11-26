35defcee501a5da473b7456129806f7aed995301
FLOURA <floura.angel.96@gmail.com>
2016-11-26 18:31:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -193,8 +193,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/me2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'me2.jpg'));
 });
 
 
